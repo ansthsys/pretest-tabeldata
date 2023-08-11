61909c8cade6863440c491c1efc445af5d62d5a9
ansthsys <azmiythufail91@gmail.com>
2023-08-11 21:51:17 +0700

    feat: user can login

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const authRouter = require("./routes/auth");
 const productRouter = require("./routes/product");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(authRouter);
 app.use(productRouter);
 
 app.listen(port, () => {
---
+++ controllers/AuthController.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { Users } = require("../models");
+
+class CartController {
+  static async login(req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Email and password is required" });
+    }
+
+    const user = await Users.findOne({
+      where: { email: email },
+    });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res
+        .status(401)
+        .json({ error: true, message: "Account not found" });
+    }
+
+    const token = jwt.sign(
+      {
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    return res.status(200).json({
+      error: false,
+      message: "Success login",
+      data: { type: "Bearer", token },
+    });
+  }
+}
+
+module.exports = CartController;
