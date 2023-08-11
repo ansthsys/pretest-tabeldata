e1fefa00be1f7a685ed03e29d526d97bd614cdcb
ansthsys <azmiythufail91@gmail.com>
2023-08-11 23:32:35 +0700

    feat: add product to cart

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,4 +1,4 @@
-const { UserCarts } = require("../models");
+const { UserCarts, Products } = require("../models");
 
 class CartController {
   static async index(req, res) {
@@ -10,6 +10,36 @@ class CartController {
     return res
       .status(200)
       .json({ error: false, message: "Get all carts", data: carts });
+  }
+
+  static async store(req, res) {
+    const { productId, quantity } = req.body;
+
+    if (!productId || !quantity || typeof quantity !== "number") {
+      return res
+        .status(400)
+        .json({ error: true, message: "productId and quantity not valid" });
+    }
+
+    const product = await Products.findByPk(productId);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ error: true, message: "Product not found" });
+    }
+
+    const cart = await UserCarts.create({
+      userId: req.user.id,
+      amount: product.price * quantity,
+      checkouted: false,
+      productId: product.id,
+      quantity,
+    });
+
+    return res
+      .status(201)
+      .json({ error: false, message: "Cart created", data: cart });
   }
 }
 
--- routes/cart.js
+++ routes/cart.js
@@ -4,5 +4,6 @@ const AuthMiddleware = require("../middleware/authenticate");
 const CartController = require("../controllers/CartController");
 
 router.get("/carts/", AuthMiddleware, CartController.index);
+router.post("/carts/", AuthMiddleware, CartController.store);
 
 module.exports = router;
