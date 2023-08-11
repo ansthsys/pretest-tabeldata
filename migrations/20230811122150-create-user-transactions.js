07c624486a976d7696b73d51fe7c65dc8af7f7ef
ansthsys <azmiythufail91@gmail.com>
2023-08-11 19:25:39 +0700

    feat: create user transaction migration

---
+++ migrations/20230811122150-create-user-transactions.js
@@ -0,0 +1,48 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("UserTransactions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      invoiceId: {
+        type: Sequelize.STRING,
+      },
+      orderDate: {
+        type: Sequelize.DATE,
+      },
+      cartsId: {
+        type: Sequelize.STRING,
+      },
+      totalAmount: {
+        type: Sequelize.DECIMAL,
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ["paid", "unpaid", "delivered", "canceled", "expired"],
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("UserTransactions");
+  },
+};
---
+++ models/usertransactions.js
@@ -0,0 +1,38 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class UserTransactions extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  UserTransactions.init(
+    {
+      userId: DataTypes.INTEGER,
+      invoiceId: DataTypes.STRING,
+      orderDate: DataTypes.DATE,
+      cartsId: DataTypes.STRING,
+      totalAmount: DataTypes.DECIMAL,
+      status: DataTypes.ENUM,
+    },
+    {
+      sequelize,
+      modelName: "UserTransactions",
+    }
+  );
+
+  UserTransactions.associate = function (models) {
+    UserTransactions.belongsTo(models.Users, {
+      foreignKey: "userId",
+      as: "user",
+    });
+  };
+
+  return UserTransactions;
+};
